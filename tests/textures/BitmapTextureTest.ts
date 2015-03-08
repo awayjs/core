1eebe0ffcdf188858202f8ba77a65b223a952046
rob-bateman <rob@infiniteturtles.co.uk>
2015-03-08 00:04:01 +0000

    update tests for refactors

--- tests/textures/BitmapTextureTest.ts
+++ tests/textures/BitmapTextureTest.ts
@@ -1,4 +1,4 @@
-import BitmapData			= require("awayjs-core/lib/base/BitmapData");
+import BitmapData			= require("awayjs-core/lib/data/BitmapData");
 import Rectangle			= require("awayjs-core/lib/geom/Rectangle");
 import URLLoader			= require("awayjs-core/lib/net/URLLoader");
 import URLLoaderDataFormat	= require("awayjs-core/lib/net/URLLoaderDataFormat");

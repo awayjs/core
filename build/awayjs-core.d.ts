980b9056cca543496d97665e0c9f0a5396e31e40
bwhiting <ben.whiting@live.co.uk>
2015-05-10 23:29:47 +0100

    update build files

--- build/awayjs-core.d.ts
+++ build/awayjs-core.d.ts
@@ -3771,70 +3771,6 @@ declare module "awayjs-core/lib/geom/Orientation3D" {
 	
 }
 
-declare module "awayjs-core/lib/geom/Plane3D" {
-	import Vector3D = require("awayjs-core/lib/geom/Vector3D");
-	class Plane3D {
-	    /**
-	     * The A coefficient of this plane. (Also the x dimension of the plane normal)
-	     */
-	    a: number;
-	    /**
-	     * The B coefficient of this plane. (Also the y dimension of the plane normal)
-	     */
-	    b: number;
-	    /**
-	     * The C coefficient of this plane. (Also the z dimension of the plane normal)
-	     */
-	    c: number;
-	    /**
-	     * The D coefficient of this plane. (Also the inverse dot product between normal and point)
-	     */
-	    d: number;
-	    _iAlignment: number;
-	    static ALIGN_ANY: number;
-	    static ALIGN_XY_AXIS: number;
-	    static ALIGN_YZ_AXIS: number;
-	    static ALIGN_XZ_AXIS: number;
-	    /**
-	     * Create a Plane3D with ABCD coefficients
-	     */
-	    constructor(a?: number, b?: number, c?: number, d?: number);
-	    /**
-	     * Fills this Plane3D with the coefficients from 3 points in 3d space.
-	     * @param p0 Vector3D
-	     * @param p1 Vector3D
-	     * @param p2 Vector3D
-	     */
-	    fromPoints(p0: Vector3D, p1: Vector3D, p2: Vector3D): void;
-	    /**
-	     * Fills this Plane3D with the coefficients from the plane's normal and a point in 3d space.
-	     * @param normal Vector3D
-	     * @param point  Vector3D
-	     */
-	    fromNormalAndPoint(normal: Vector3D, point: Vector3D): void;
-	    /**
-	     * Normalize this Plane3D
-	     * @return Plane3D This Plane3D.
-	     */
-	    normalize(): Plane3D;
-	    /**
-	     * Returns the signed distance between this Plane3D and the point p.
-	     * @param p Vector3D
-	     * @returns Number
-	     */
-	    distance(p: Vector3D): number;
-	    /**
-	     * Classify a point against this Plane3D. (in front, back or intersecting)
-	     * @param p Vector3D
-	     * @return int Plane3.FRONT or Plane3D.BACK or Plane3D.INTERSECT
-	     */
-	    classifyPoint(p: Vector3D, epsilon?: number): number;
-	    toString(): string;
-	}
-	export = Plane3D;
-	
-}
-
 declare module "awayjs-core/lib/geom/PerspectiveProjection" {
 	import Matrix3D = require("awayjs-core/lib/geom/Matrix3D");
 	import Point = require("awayjs-core/lib/geom/Point");
@@ -3963,6 +3899,70 @@ declare module "awayjs-core/lib/geom/PerspectiveProjection" {
 	    toMatrix3D(): Matrix3D;
 	}
 	export = PerspectiveProjection;
+	
+}
+
+declare module "awayjs-core/lib/geom/Plane3D" {
+	import Vector3D = require("awayjs-core/lib/geom/Vector3D");
+	class Plane3D {
+	    /**
+	     * The A coefficient of this plane. (Also the x dimension of the plane normal)
+	     */
+	    a: number;
+	    /**
+	     * The B coefficient of this plane. (Also the y dimension of the plane normal)
+	     */
+	    b: number;
+	    /**
+	     * The C coefficient of this plane. (Also the z dimension of the plane normal)
+	     */
+	    c: number;
+	    /**
+	     * The D coefficient of this plane. (Also the inverse dot product between normal and point)
+	     */
+	    d: number;
+	    _iAlignment: number;
+	    static ALIGN_ANY: number;
+	    static ALIGN_XY_AXIS: number;
+	    static ALIGN_YZ_AXIS: number;
+	    static ALIGN_XZ_AXIS: number;
+	    /**
+	     * Create a Plane3D with ABCD coefficients
+	     */
+	    constructor(a?: number, b?: number, c?: number, d?: number);
+	    /**
+	     * Fills this Plane3D with the coefficients from 3 points in 3d space.
+	     * @param p0 Vector3D
+	     * @param p1 Vector3D
+	     * @param p2 Vector3D
+	     */
+	    fromPoints(p0: Vector3D, p1: Vector3D, p2: Vector3D): void;
+	    /**
+	     * Fills this Plane3D with the coefficients from the plane's normal and a point in 3d space.
+	     * @param normal Vector3D
+	     * @param point  Vector3D
+	     */
+	    fromNormalAndPoint(normal: Vector3D, point: Vector3D): void;
+	    /**
+	     * Normalize this Plane3D
+	     * @return Plane3D This Plane3D.
+	     */
+	    normalize(): Plane3D;
+	    /**
+	     * Returns the signed distance between this Plane3D and the point p.
+	     * @param p Vector3D
+	     * @returns Number
+	     */
+	    distance(p: Vector3D): number;
+	    /**
+	     * Classify a point against this Plane3D. (in front, back or intersecting)
+	     * @param p Vector3D
+	     * @return int Plane3.FRONT or Plane3D.BACK or Plane3D.INTERSECT
+	     */
+	    classifyPoint(p: Vector3D, epsilon?: number): number;
+	    toString(): string;
+	}
+	export = Plane3D;
 	
 }
 
@@ -6939,6 +6939,27 @@ declare module "awayjs-core/lib/projections/IProjection" {
 	
 }
 
+declare module "awayjs-core/lib/projections/OrthographicOffCenterProjection" {
+	import Vector3D = require("awayjs-core/lib/geom/Vector3D");
+	import ProjectionBase = require("awayjs-core/lib/projections/ProjectionBase");
+	class OrthographicOffCenterProjection extends ProjectionBase {
+	    private _minX;
+	    private _maxX;
+	    private _minY;
+	    private _maxY;
+	    constructor(minX: number, maxX: number, minY: number, maxY: number);
+	    minX: number;
+	    maxX: number;
+	    minY: number;
+	    maxY: number;
+	    unproject(nX: number, nY: number, sZ: number): Vector3D;
+	    clone(): ProjectionBase;
+	    pUpdateMatrix(): void;
+	}
+	export = OrthographicOffCenterProjection;
+	
+}
+
 declare module "awayjs-core/lib/projections/ObliqueNearPlaneProjection" {
 	import Plane3D = require("awayjs-core/lib/geom/Plane3D");
 	import IProjection = require("awayjs-core/lib/projections/IProjection");
@@ -6958,27 +6979,6 @@ declare module "awayjs-core/lib/projections/ObliqueNearPlaneProjection" {
 	    pUpdateMatrix(): void;
 	}
 	export = ObliqueNearPlaneProjection;
-	
-}
-
-declare module "awayjs-core/lib/projections/OrthographicOffCenterProjection" {
-	import Vector3D = require("awayjs-core/lib/geom/Vector3D");
-	import ProjectionBase = require("awayjs-core/lib/projections/ProjectionBase");
-	class OrthographicOffCenterProjection extends ProjectionBase {
-	    private _minX;
-	    private _maxX;
-	    private _minY;
-	    private _maxY;
-	    constructor(minX: number, maxX: number, minY: number, maxY: number);
-	    minX: number;
-	    maxX: number;
-	    minY: number;
-	    maxY: number;
-	    unproject(nX: number, nY: number, sZ: number): Vector3D;
-	    clone(): ProjectionBase;
-	    pUpdateMatrix(): void;
-	}
-	export = OrthographicOffCenterProjection;
 	
 }
 

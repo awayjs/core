9cff81fdb8916cbfb918953e91594bfef76dc1dc
rob-bateman <rob@infiniteturtles.co.uk>
2016-02-18 03:56:11 +0000

    fix to textureatlasparser to use fractional dimensions

--- lib/library/IAbstractionPool.ts
+++ lib/library/IAbstractionPool.ts
@@ -1,8 +1,9 @@
 import IAsset						= require("awayjs-core/lib/library/IAsset");
+import AbstractionBase				= require("awayjs-core/lib/library/AbstractionBase");
 
 interface IAbstractionPool
 {
-	getAbstraction(asset:IAsset);
+	getAbstraction(asset:IAsset):AbstractionBase;
 	clearAbstraction(asset:IAsset);
 }
 

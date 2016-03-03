0943b86c488c9629f3652fcd6dc05d86bcee8d47
rob-bateman <rob@infiniteturtles.co.uk>
2016-03-03 02:27:12 +0000

    update build files

--- build/awayjs-core.js
+++ build/awayjs-core.js
@@ -257,14 +257,16 @@ var AttributesBuffer = require("awayjs-core/lib/attributes/AttributesBuffer");
 var AssetBase = require("awayjs-core/lib/library/AssetBase");
 var AttributesView = (function (_super) {
     __extends(AttributesView, _super);
-    function AttributesView(arrayClass, dimensions, attributesBufferCount) {
+    function AttributesView(arrayClass, dimensions, attributesBufferCount, unsigned) {
         if (attributesBufferCount === void 0) { attributesBufferCount = 0; }
+        if (unsigned === void 0) { unsigned = false; }
         _super.call(this);
         this._arrayClass = arrayClass;
         this._size = arrayClass.BYTES_PER_ELEMENT;
         this._dimensions = dimensions;
         this._attributesBuffer = (attributesBufferCount instanceof AttributesBuffer) ? attributesBufferCount : new AttributesBuffer(this._dimensions * this._size, attributesBufferCount);
         this._attributesBuffer._addView(this);
+        this._unsigned = unsigned;
     }
     Object.defineProperty(AttributesView.prototype, "assetType", {
         /**
@@ -316,6 +318,13 @@ var AttributesView = (function (_super) {
             //reset view
             this._attributesBuffer._removeView(this);
             this._attributesBuffer._addView(this);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(AttributesView.prototype, "unsigned", {
+        get: function () {
+            return this._unsigned;
         },
         enumerable: true,
         configurable: true
@@ -384,7 +393,142 @@ var AttributesView = (function (_super) {
 })(AssetBase);
 module.exports = AttributesView;
 
-},{"awayjs-core/lib/attributes/AttributesBuffer":"awayjs-core/lib/attributes/AttributesBuffer","awayjs-core/lib/library/AssetBase":"awayjs-core/lib/library/AssetBase"}],"awayjs-core/lib/attributes/Byte4Attributes":[function(require,module,exports){
+},{"awayjs-core/lib/attributes/AttributesBuffer":"awayjs-core/lib/attributes/AttributesBuffer","awayjs-core/lib/library/AssetBase":"awayjs-core/lib/library/AssetBase"}],"awayjs-core/lib/attributes/Byte1Attributes":[function(require,module,exports){
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var AttributesView = require("awayjs-core/lib/attributes/AttributesView");
+var Byte1Attributes = (function (_super) {
+    __extends(Byte1Attributes, _super);
+    function Byte1Attributes(attributesBufferLength, unsigned) {
+        if (unsigned === void 0) { unsigned = true; }
+        _super.call(this, unsigned ? Uint8Array : Int8Array, 1, attributesBufferLength, unsigned);
+    }
+    Object.defineProperty(Byte1Attributes.prototype, "assetType", {
+        /**
+         *
+         * @returns {string}
+         */
+        get: function () {
+            return Byte1Attributes.assetType;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Byte1Attributes.prototype.set = function (values, offset) {
+        if (offset === void 0) { offset = 0; }
+        _super.prototype.set.call(this, values, offset);
+    };
+    Byte1Attributes.prototype.get = function (count, offset) {
+        if (offset === void 0) { offset = 0; }
+        return _super.prototype.get.call(this, count, offset);
+    };
+    Byte1Attributes.prototype._internalClone = function (attributesBuffer) {
+        return (this._cloneCache = new Byte1Attributes(attributesBuffer, this._arrayClass == Uint8Array));
+    };
+    Byte1Attributes.prototype.clone = function (attributesBuffer) {
+        if (attributesBuffer === void 0) { attributesBuffer = null; }
+        return _super.prototype.clone.call(this, attributesBuffer);
+    };
+    Byte1Attributes.assetType = "[attributes Byte1Attributes]";
+    return Byte1Attributes;
+})(AttributesView);
+module.exports = Byte1Attributes;
+
+},{"awayjs-core/lib/attributes/AttributesView":"awayjs-core/lib/attributes/AttributesView"}],"awayjs-core/lib/attributes/Byte2Attributes":[function(require,module,exports){
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var AttributesView = require("awayjs-core/lib/attributes/AttributesView");
+var Byte2Attributes = (function (_super) {
+    __extends(Byte2Attributes, _super);
+    function Byte2Attributes(attributesBufferLength, unsigned) {
+        if (unsigned === void 0) { unsigned = true; }
+        _super.call(this, unsigned ? Uint8Array : Int8Array, 2, attributesBufferLength, unsigned);
+    }
+    Object.defineProperty(Byte2Attributes.prototype, "assetType", {
+        /**
+         *
+         * @returns {string}
+         */
+        get: function () {
+            return Byte2Attributes.assetType;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Byte2Attributes.prototype.set = function (values, offset) {
+        if (offset === void 0) { offset = 0; }
+        _super.prototype.set.call(this, values, offset);
+    };
+    Byte2Attributes.prototype.get = function (count, offset) {
+        if (offset === void 0) { offset = 0; }
+        return _super.prototype.get.call(this, count, offset);
+    };
+    Byte2Attributes.prototype._internalClone = function (attributesBuffer) {
+        return (this._cloneCache = new Byte2Attributes(attributesBuffer, this._arrayClass == Uint8Array));
+    };
+    Byte2Attributes.prototype.clone = function (attributesBuffer) {
+        if (attributesBuffer === void 0) { attributesBuffer = null; }
+        return _super.prototype.clone.call(this, attributesBuffer);
+    };
+    Byte2Attributes.assetType = "[attributes Byte2Attributes]";
+    return Byte2Attributes;
+})(AttributesView);
+module.exports = Byte2Attributes;
+
+},{"awayjs-core/lib/attributes/AttributesView":"awayjs-core/lib/attributes/AttributesView"}],"awayjs-core/lib/attributes/Byte3Attributes":[function(require,module,exports){
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var AttributesView = require("awayjs-core/lib/attributes/AttributesView");
+var Byte3Attributes = (function (_super) {
+    __extends(Byte3Attributes, _super);
+    function Byte3Attributes(attributesBufferLength, unsigned) {
+        if (unsigned === void 0) { unsigned = true; }
+        _super.call(this, unsigned ? Uint8Array : Int8Array, 3, attributesBufferLength, unsigned);
+    }
+    Object.defineProperty(Byte3Attributes.prototype, "assetType", {
+        /**
+         *
+         * @returns {string}
+         */
+        get: function () {
+            return Byte3Attributes.assetType;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Byte3Attributes.prototype.set = function (values, offset) {
+        if (offset === void 0) { offset = 0; }
+        _super.prototype.set.call(this, values, offset);
+    };
+    Byte3Attributes.prototype.get = function (count, offset) {
+        if (offset === void 0) { offset = 0; }
+        return _super.prototype.get.call(this, count, offset);
+    };
+    Byte3Attributes.prototype._internalClone = function (attributesBuffer) {
+        return (this._cloneCache = new Byte3Attributes(attributesBuffer, this._arrayClass == Uint8Array));
+    };
+    Byte3Attributes.prototype.clone = function (attributesBuffer) {
+        if (attributesBuffer === void 0) { attributesBuffer = null; }
+        return _super.prototype.clone.call(this, attributesBuffer);
+    };
+    Byte3Attributes.assetType = "[attributes Byte3Attributes]";
+    return Byte3Attributes;
+})(AttributesView);
+module.exports = Byte3Attributes;
+
+},{"awayjs-core/lib/attributes/AttributesView":"awayjs-core/lib/attributes/AttributesView"}],"awayjs-core/lib/attributes/Byte4Attributes":[function(require,module,exports){
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -396,7 +540,7 @@ var Byte4Attributes = (function (_super) {
     __extends(Byte4Attributes, _super);
     function Byte4Attributes(attributesBufferLength, unsigned) {
         if (unsigned === void 0) { unsigned = true; }
-        _super.call(this, unsigned ? Uint8Array : Int8Array, 4, attributesBufferLength);
+        _super.call(this, unsigned ? Uint8Array : Int8Array, 4, attributesBufferLength, unsigned);
     }
     Object.defineProperty(Byte4Attributes.prototype, "assetType", {
         /**
@@ -617,7 +761,7 @@ var Short2Attributes = (function (_super) {
     __extends(Short2Attributes, _super);
     function Short2Attributes(attributesBufferLength, unsigned) {
         if (unsigned === void 0) { unsigned = true; }
-        _super.call(this, unsigned ? Uint16Array : Int16Array, 2, attributesBufferLength);
+        _super.call(this, unsigned ? Uint16Array : Int16Array, 2, attributesBufferLength, unsigned);
     }
     Object.defineProperty(Short2Attributes.prototype, "assetType", {
         /**
@@ -662,7 +806,7 @@ var Short3Attributes = (function (_super) {
     __extends(Short3Attributes, _super);
     function Short3Attributes(attributesBufferLength, unsigned) {
         if (unsigned === void 0) { unsigned = true; }
-        _super.call(this, unsigned ? Uint16Array : Int16Array, 3, attributesBufferLength);
+        _super.call(this, unsigned ? Uint16Array : Int16Array, 3, attributesBufferLength, unsigned);
     }
     Object.defineProperty(Short3Attributes.prototype, "assetType", {
         /**

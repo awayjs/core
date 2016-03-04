a2c2c82f18fe5623814e163dda553870dc4e17df
rob-bateman <rob@infiniteturtles.co.uk>
2016-03-04 00:04:35 +0000

    use absolute maxlength in ByteArray constructor rather than multiple of FF

--- lib/utils/ByteArray.ts
+++ lib/utils/ByteArray.ts
@@ -10,7 +10,7 @@ class ByteArray extends ByteArrayBase
 	{
 		super();
 		this._mode = "Typed array";
-		this.maxlength = Math.max((maxlength + 255) & (~255), 4);
+		this.maxlength = maxlength < 4? 4 : maxlength;
 		this.arraybytes = new ArrayBuffer(this.maxlength);
 		this.unalignedarraybytestemp = new ArrayBuffer(16);
 	}

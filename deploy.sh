080522aa8ae4a94d76d3ccadc3794b46949eb369
Alejandro Santander <palebluedot@gmail.com>
2017-02-24 12:45:58 -0300

    Skip docs compilation for any branch other than dev

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,13 @@ SOURCE_BRANCH="dev"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-    echo "Compiling..."
+    echo "Compiling docs..."
     npm run docs
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
+    echo "Not a dev branch commit, skipping docs deploy."
     exit 0
 fi
 

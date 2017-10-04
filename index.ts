awayjs/graphics

updated code to use graphics module

index.ts
@@ -1,0 +1,84 @@
+export {ParticleData}					from "./lib/animators/data/ParticleData";
+export {AnimationNodeBase}				from "./lib/animators/nodes/AnimationNodeBase";
+export {IAnimationSet}					from "./lib/animators/IAnimationSet";
+export {IAnimator}						from "./lib/animators/IAnimator";
+
+export {IContainerNode}					from "./lib/base/IContainerNode";
+export {IEntity}						from "./lib/base/IEntity";
+export {IMaterial}						from "./lib/base/IMaterial";
+export {INode}							from "./lib/base/INode";
+export {IRenderable}						from "./lib/base/IRenderable";
+export {Style}							from "./lib/base/Style";
+export {Transform}						from "./lib/base/Transform";
+export {TraverserBase}					from "./lib/base/TraverserBase";
+
+export {CapsStyle}						from "./lib/draw/CapsStyle";
+export {DrawMode}						from "./lib/draw/DrawMode";
+export {GradientType}					from "./lib/draw/GradientType";
+export {GraphicsFactoryFills} 			from "./lib/draw/GraphicsFactoryFills";
+export {GraphicsFactoryHelper} 			from "./lib/draw/GraphicsFactoryHelper";
+export {GraphicsFactoryStrokes} 			from "./lib/draw/GraphicsFactoryStrokes";
+export {GraphicsFillStyle} 			    from "./lib/draw/GraphicsFillStyle";
+export {GraphicsStrokeStyle}			    from "./lib/draw/GraphicsStrokeStyle";
+export {GraphicsPath}					from "./lib/draw/GraphicsPath";
+export {GraphicsPathCommand}				from "./lib/draw/GraphicsPathCommand";
+export {GraphicsPathWinding}				from "./lib/draw/GraphicsPathWinding";
+export {IGraphicsData}					from "./lib/draw/IGraphicsData";
+export {InterpolationMethod}				from "./lib/draw/InterpolationMethod";
+export {JointStyle}						from "./lib/draw/JointStyle";
+export {LineScaleMode}					from "./lib/draw/LineScaleMode";
+export {PixelSnapping}					from "./lib/draw/PixelSnapping";
+export {SpreadMethod}					from "./lib/draw/SpreadMethod";
+export {TriangleCulling}					from "./lib/draw/TriangleCulling";
+
+export {ElementsEvent}					from "./lib/events/ElementsEvent";
+export {MaterialEvent}					from "./lib/events/MaterialEvent";
+export {RenderableEvent}				from "./lib/events/RenderableEvent";
+export {StyleEvent}						from "./lib/events/StyleEvent";
+export {TransformEvent}					from "./lib/events/TransformEvent";
+
+export {ElementsBase}					from "./lib/elements/ElementsBase";
+export {ElementsType}					from "./lib/elements/ElementsType";
+export {LineElements}					from "./lib/elements/LineElements";
+export {TriangleElements}				from "./lib/elements/TriangleElements";
+
+export {BitmapImage2D}					from "./lib/image/BitmapImage2D";
+export {BitmapImageChannel}				from "./lib/image/BitmapImageChannel";
+export {BitmapImageCube}				from "./lib/image/BitmapImageCube";
+export {BlendMode}						from "./lib/image/BlendMode";
+export {CPUCanvas}						from "./lib/image/CPUCanvas";
+export {CPURenderingContext2D}			from "./lib/image/CPURenderingContext2D";
+export {IImageCanvas}					from "./lib/image/IImageCanvas";
+export {Image2D}						from "./lib/image/Image2D";
+export {ImageBase}						from "./lib/image/ImageBase";
+export {ImageCube}						from "./lib/image/ImageCube";
+export {ImageData}						from "./lib/image/ImageData";
+export {Sampler2D}						from "./lib/image/Sampler2D";
+export {SamplerBase}					from "./lib/image/SamplerBase";
+export {SamplerCube}					from "./lib/image/SamplerCube";
+export {SpecularImage2D}				from "./lib/image/SpecularImage2D";
+
+export {DefaultMaterialManager}			from "./lib/managers/DefaultMaterialManager";
+
+export {BasicMaterial}					from "./lib/materials/BasicMaterial";
+export {MaterialBase}					from "./lib/materials/MaterialBase";
+
+export {Image2DParser}					from "./lib/parsers/Image2DParser";
+export {ImageCubeParser}				from "./lib/parsers/ImageCubeParser";
+export {TextureAtlasParser}				from "./lib/parsers/TextureAtlasParser";
+
+export {PickingCollision}				from "./lib/pick/PickingCollision";
+
+export {MappingMode}						from "./lib/textures/MappingMode";
+export {Single2DTexture}					from "./lib/textures/Single2DTexture";
+export {SingleCubeTexture}				from "./lib/textures/SingleCubeTexture";
+export {TextureBase}						from "./lib/textures/TextureBase";
+
+export {BitmapImageUtils}				from "./lib/utils/BitmapImageUtils";
+export {ElementsUtils}					from "./lib/utils/ElementsUtils";
+export {HitTestCache}					from "./lib/utils/HitTestCache";
+export {ImageUtils}						from "./lib/utils/ImageUtils";
+export {MipmapGenerator}				from "./lib/utils/MipmapGenerator";
+
+export {Graphic}						from "./lib/Graphic";
+export {Graphics}						from "./lib/Graphics";

awayjs/core

index.ts
@@ -1,34 +1,106 @@
-import * as attributes				from "./lib/attributes";
-import * as audio					from "./lib/audio";
-import * as errors					from "./lib/errors";
-import * as events					from "./lib/events";
-import * as geom					from "./lib/geom";
-import * as image					from "./lib/image";
-import * as library					from "./lib/library";
-import * as managers				from "./lib/managers";
-import * as net						from "./lib/net";
-import * as parsers					from "./lib/parsers";
-import * as projections				from "./lib/projections";
-import * as ui						from "./lib/ui";
-import * as utils					from "./lib/utils";
+export {AttributesView}					from "./lib/attributes/AttributesView";
+export {AttributesBuffer}				from "./lib/attributes/AttributesBuffer";
+export {Byte4Attributes}				from "./lib/attributes/Byte4Attributes";
+export {Float1Attributes}				from "./lib/attributes/Float1Attributes";
+export {Float2Attributes}				from "./lib/attributes/Float2Attributes";
+export {Float3Attributes}				from "./lib/attributes/Float3Attributes";
+export {Float4Attributes}				from "./lib/attributes/Float4Attributes";
+export {Short2Attributes}				from "./lib/attributes/Short2Attributes";
+export {Short3Attributes}				from "./lib/attributes/Short3Attributes";
 
-library.Loader.enableParser(parsers.Image2DParser);
-library.Loader.enableParser(parsers.ImageCubeParser);
-library.Loader.enableParser(parsers.TextureAtlasParser);
-library.Loader.enableParser(parsers.WaveAudioParser);
+export {WaveAudio}						from "./lib/audio/WaveAudio";
 
-export {
-	attributes,
-	audio,
-	errors,
-	events,
-	geom,
-	image,
-	library,
-	managers,
-	net,
-	parsers,
-	projections,
-	ui,
-	utils
-}
+export {AbstractMethodError}			from "./lib/errors/AbstractMethodError";
+export {ArgumentError}					from "./lib/errors/ArgumentError";
+export {DocumentError}					from "./lib/errors/DocumentError";
+export {ErrorBase}						from "./lib/errors/ErrorBase";
+export {PartialImplementationError}		from "./lib/errors/PartialImplementationError";
+export {RangeError}						from "./lib/errors/RangeError";
+
+export {AssetEvent}						from "./lib/events/AssetEvent";
+export {EventBase}						from "./lib/events/EventBase";
+export {EventDispatcher}				from "./lib/events/EventDispatcher";
+export {IEventDispatcher}				from "./lib/events/IEventDispatcher";
+export {LoaderEvent}					from "./lib/events/LoaderEvent";
+export {ParserEvent}					from "./lib/events/ParserEvent";
+export {ProjectionEvent}				from "./lib/events/ProjectionEvent";
+export {TimerEvent}						from "./lib/events/TimerEvent";
+export {URLLoaderEvent}					from "./lib/events/URLLoaderEvent";
+
+export {Box}							from "./lib/geom/Box";
+export {ColorTransform}					from "./lib/geom/ColorTransform";
+export {MathConsts}						from "./lib/geom/MathConsts";
+export {Matrix}							from "./lib/geom/Matrix";
+export {Matrix3D}						from "./lib/geom/Matrix3D";
+export {Orientation3D}					from "./lib/geom/Orientation3D";
+export {Plane3D}						from "./lib/geom/Plane3D";
+export {PlaneClassification}			from "./lib/geom/PlaneClassification";
+export {Point}							from "./lib/geom/Point";
+export {PoissonLookup}					from "./lib/geom/PoissonLookup";
+export {Quaternion}						from "./lib/geom/Quaternion";
+export {Rectangle}						from "./lib/geom/Rectangle";
+export {Sphere}							from "./lib/geom/Sphere";
+export {Vector3D}						from "./lib/geom/Vector3D";
+
+export {AbstractionBase}				from "./lib/library/AbstractionBase";
+export {AssetBase}						from "./lib/library/AssetBase";
+export {AssetLibrary}					from "./lib/library/AssetLibrary";
+export {AssetLibraryBundle}				from "./lib/library/AssetLibraryBundle";
+export {AssetLibraryIterator}			from "./lib/library/AssetLibraryIterator";
+export {ConflictPrecedence}				from "./lib/library/ConflictPrecedence";
+export {ConflictStrategy}				from "./lib/library/ConflictStrategy";
+export {ConflictStrategyBase}			from "./lib/library/ConflictStrategyBase";
+export {ErrorConflictStrategy}			from "./lib/library/ErrorConflictStrategy";
+export {IAbstractionPool}				from "./lib/library/IAbstractionPool";
+export {IAsset}							from "./lib/library/IAsset";
+export {IAssetClass}					from "./lib/library/IAssetClass";
+export {IDUtil}							from "./lib/library/IDUtil";
+export {IgnoreConflictStrategy}			from "./lib/library/IgnoreConflictStrategy";
+export {Loader}							from "./lib/library/Loader";
+export {LoaderContext}					from "./lib/library/LoaderContext";
+export {LoaderInfo}						from "./lib/library/LoaderInfo";
+export {NumSuffixConflictStrategy}		from "./lib/library/NumSuffixConflictStrategy";
+
+export {AudioManager}					from "./lib/managers/AudioManager";
+export {IAudioChannel}					from "./lib/managers/IAudioChannel";
+export {IAudioChannelClass}				from "./lib/managers/IAudioChannelClass";
+export {StreamingAudioChannel}			from "./lib/managers/StreamingAudioChannel";
+export {WebAudioChannel}				from "./lib/managers/WebAudioChannel";
+
+export {CrossDomainPolicy}				from "./lib/net/CrossDomainPolicy";
+export {URLLoader}						from "./lib/net/URLLoader";
+export {URLLoaderDataFormat}			from "./lib/net/URLLoaderDataFormat";
+export {URLRequest}						from "./lib/net/URLRequest";
+export {URLRequestMethod}				from "./lib/net/URLRequestMethod";
+export {URLVariables}					from "./lib/net/URLVariables";
+
+export {ParserBase}						from "./lib/parsers/ParserBase";
+export {ParserDataFormat}				from "./lib/parsers/ParserDataFormat";
+export {ParserUtils}					from "./lib/parsers/ParserUtils";
+export {ResourceDependency}				from "./lib/parsers/ResourceDependency";
+export {WaveAudioParser}				from "./lib/parsers/WaveAudioParser";
+
+export {CoordinateSystem}				from "./lib/projections/CoordinateSystem";
+export {FreeMatrixProjection}			from "./lib/projections/FreeMatrixProjection";
+export {IProjection}					from "./lib/projections/IProjection";
+export {ObliqueNearPlaneProjection}		from "./lib/projections/ObliqueNearPlaneProjection";
+export {OrthographicOffCenterProjection}	from "./lib/projections/OrthographicOffCenterProjection";
+export {OrthographicProjection}			from "./lib/projections/OrthographicProjection";
+export {PerspectiveProjection}			from "./lib/projections/PerspectiveProjection";
+export {ProjectionBase}					from "./lib/projections/ProjectionBase";
+
+export {Keyboard}						from "./lib/ui/Keyboard";
+
+export {ByteArray}						from "./lib/utils/ByteArray";
+export {Byte32Array}					from "./lib/utils/Byte32Array";
+export {ByteArrayBase}					from "./lib/utils/ByteArrayBase";
+export {ByteArrayBuffer}				from "./lib/utils/ByteArrayBuffer";
+export {ColorUtils}						from "./lib/utils/ColorUtils";
+export {CSS}							from "./lib/utils/CSS";
+export {Debug}							from "./lib/utils/Debug";
+export {Extensions}						from "./lib/utils/Extensions";
+export {getTimer}						from "./lib/utils/getTimer";
+export {IArrayBufferViewClass}			from "./lib/utils/IArrayBufferViewClass";
+export {RequestAnimationFrame}			from "./lib/utils/RequestAnimationFrame";
+export {Timer}							from "./lib/utils/Timer";
+export {XmlUtils}						from "./lib/utils/XmlUtils";
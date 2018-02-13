b90ad22a0ba43124088401c683dbde99ae415499
80prozent <80prozent@differentdesign.de>
2018-02-13 19:32:08 +0100

    add BuildMode helper-class

---
+++ lib/utils/BuildMode.ts
@@ -0,0 +1,14 @@
+
+// helper class for places that needs different behaviour based on if we are building for AVM1 or not
+// hopefully in future this will no longer be needed, but for now its the best we can do to make adjustments per project easier / clearer
+
+export class BuildMode {
+	// used when building AwayJS to use with AVM1
+	public static AVM1:string = "avm1";
+
+	// used when building standart AwayJS (not for AVM1)
+	public static STANDART:string = "standart";
+
+	public static mode:string=BuildMode.STANDART;
+
+}

163cf8904d6bf22da6599103666eaf83981bf222
80prozent <80prozent@differentdesign.de>
2018-06-11 20:43:23 +0200

    test change for testing npm version patch

--- consoleLogVersion.js
+++ consoleLogVersion.js
@@ -9,11 +9,11 @@ var fs = require("fs");
 // with the new version number from package.json
 
 // update dist/index.js with the new content
-console.log("update dist/index.js with version:", process.env.npm_package_version);
+console.log("update src/index.ts with version:", process.env.npm_package_version);
 
-fs.readFile("./dist/index.js", 'utf8', function(err, data) {
+fs.readFile("./src/index.ts", 'utf8', function(err, data) {
     if (err) throw err;
-    data = data.replace("#BUILD_VIA_NPM_VERSION_PATCH_TO_DISPLAY_VERSION_HERE#", process.env.npm_package_version);
+    data = data.replace(/\- (*?))\"\;/g, process.env.npm_package_version);//#BUILD_VIA_NPM_VERSION_PATCH_TO_DISPLAY_VERSION_HERE#", process.env.npm_package_version);
     fs.writeFile("./dist/index.js", data, function(err) {
         if (err) throw err;    
         console.log("Updated ./dist/index.js with inserted version ", process.env.npm_package_version);

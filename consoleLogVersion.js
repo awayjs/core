33d5350466975df2fb71fe8899110ccd32729b2d
80prozent <80prozent@differentdesign.de>
2018-06-11 19:05:19 +0200

    test change for testing npm version patch

--- consoleLogVersion.js
+++ consoleLogVersion.js
@@ -6,7 +6,7 @@ fs.readFile("./dist/index.js", 'utf8', function(err, data) {
     if (err) throw err;
     indexJSData=data.replace("#BUILD_VIA_NPM_VERSION_PATCH_TO_DISPLAY_VERSION_HERE#", process.env.npm_package_version);
     console.log(indexJSData);
-    fs.writeFile("/tmp/test", data, function(err) {
+    fs.writeFile("./dist/index.js", data, function(err) {
         if (err) throw err;    
         console.log("Updated ./dist/index.js with inserted version ", process.env.npm_package_version);
     }); 

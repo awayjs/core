72247f9ac835546d68692d7d6844574bb3455778
80prozent <80prozent@differentdesign.de>
2018-06-11 19:03:38 +0200

    test change for testing npm version patch

--- consoleLogVersion.js
+++ consoleLogVersion.js
@@ -1,52 +1,13 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-//var path = require("path");
-/*
-function readdir(dir, prefix, outputDir, result) {
-    if (prefix === void 0) { prefix = ''; }
-    if (outputDir === void 0) { outputDir = ""; }
-    if (result === void 0) { result = []; }
-    //console.log("test dir", dir );
-    if (!fs.existsSync(outputDir))
-        fs.mkdirSync(outputDir);
-    fs.readdirSync(dir).forEach(function (file) {
-        var fileName = path.join(prefix, file);
-        var filePath = path.join(dir, file);
-        var outPath = path.join(outputDir, file);
-        if (!fs.statSync(filePath).isDirectory()) {
-            if (path.extname(file) === ".swf") {
-                var file1 = fs.createReadStream(filePath, { flags: 'r', encoding: "binary" });
-                var dest = fs.createWriteStream(outPath, { flags: 'w', encoding: "binary" });
-                file1.pipe(dest, { end: false });
-                result.push(outPath);
-            }
-        }
-        else {
-            //console.log("test file", file );
-            if (file != "source" && file != "Source" && file != "paper-based" && file != "Paper-based") {
-                readdir(filePath, fileName, outPath, result);
-            }
-        }
-    });
-    return result;
-}*/
+var indexJSData="";
 fs.readFile("./dist/index.js", 'utf8', function(err, data) {
-  if (err) throw err;
-  console.log('OK: ');
-  //data=dat.replace("")
-  console.log(data)
+    if (err) throw err;
+    indexJSData=dat.replace("#BUILD_VIA_NPM_VERSION_PATCH_TO_DISPLAY_VERSION_HERE#", process.env.npm_package_version);
+    console.log(indexJSData);
+    fs.writeFile("/tmp/test", "Hey there!", function(err) {
+        if (err) throw err;    
+        console.log("Updated ./dist/index.js with inserted version ", process.env.npm_package_version);
+    }); 
 });           
-console.log(process.env.npm_package_version);
-/*var len = output.length;
-var outStr = "[";
-for (var i = 0; i < len; i++) {
-    outStr += '{"name": "' + output[i].replace("d:\\away2017\\mw_repo\\mw_swf\\bin", ".").replace(/\\/g, "/").replace(".swf", "") + '", "props":{}},\n';
-}
-outStr += "]";
-fs.writeFile("./bin/mathwhizz_swfs_new.txt", outStr, function (err) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-});*/

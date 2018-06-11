cd7a2b84440524f9ccc0c5ef4bbe8fb0bcb076ca
80prozent <80prozent@differentdesign.de>
2018-06-11 21:47:00 +0200

    'addVersionToIndex'

--- copyVersionToIndex.js
+++ copyVersionToIndex.js
@@ -13,8 +13,10 @@ console.log("update src/index.ts with version:", process.env.npm_package_version
 
 fs.readFile("./index.ts", 'utf8', function(err, data) {
     if (err) throw err;
-    var re = /(.*console\.log\(\"AwayJS\s\-\s[a-zA-Z]\s\-\s)(.*)(\"\)\;.*)/;
+    var re = /(.*[a-zA-Z]\s\-\s)(.*)(\"\)\;.*)/;
+    //console.log("before", data)
     data = data.replace(re, "$1"+process.env.npm_package_version+"$3");//#BUILD_VIA_NPM_VERSION_PATCH_TO_DISPLAY_VERSION_HERE#", process.env.npm_package_version);
+    //console.log("after", data)
     fs.writeFile("./index.ts", data, function(err) {
         if (err) throw err;    
         console.log("Updated ./index.ts with inserted version ", process.env.npm_package_version);
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.log("AwayJS - Core - 0.0.40");
+console.log("AwayJS - Core - 0.7.46");
 
 export {WaveAudio}						from "./lib/audio/WaveAudio";
 

648bf5a8c28eb4ef579114812215f840f3fe07eb
80prozent <80prozent@differentdesign.de>
2018-06-11 16:24:27 +0200

    fix bug in WaveAudio.isPlaying

--- consoleLogVersion.js
+++ consoleLogVersion.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
-var fs = require("fs-extra");
-var path = require("path");
+//var fs = require("fs-extra");
+//var path = require("path");
 /*
 function readdir(dir, prefix, outputDir, result) {
     if (prefix === void 0) { prefix = ''; }

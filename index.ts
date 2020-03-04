c4757eaf5c88107b654795fd59dc0db03539d0c7
80prozent <80prozent@differentdesign.de>
2020-03-04 22:58:28 +0100

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.debug("AwayJS - Core - 0.8.17");
+console.debug("AwayJS - Core - 0.8.18");
 
 export {WaveAudio}						from "./lib/audio/WaveAudio";
 export {WaveAudioData}					from "./lib/audio/WaveAudio";

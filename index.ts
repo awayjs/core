f353f84ea174053ca780b766d0661dd71c1f8041
Rob Bateman <rob.bateman@gmail.com>
2020-03-04 12:04:54 +0000

    update version number in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 
-console.debug("AwayJS - Core - 0.8.16");
+console.debug("AwayJS - Core - 0.8.17");
 
 export {WaveAudio}						from "./lib/audio/WaveAudio";
 export {WaveAudioData}						from "./lib/audio/WaveAudio";

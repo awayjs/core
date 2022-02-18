bfa5128306f45ca82b5ec42269ac3f3176f330d4
Joshua Tenner <tenner.joshua@gmail.com>
2022-02-18 17:38:36 -0500

    Matrix concat

--- assembly/geom/Matrix.ts
+++ assembly/geom/Matrix.ts
@@ -46,31 +46,85 @@ export function Matrix_free(matrix: Matrix): void {
 }
 
 // @ts-ignore
-@lazy let adMask = f32x4(1, 0, 0, 1);
+@lazy const adzzSwizzle: v128 = v128(0, 1, 2, 3, 12, 13, 14, 15, 0, 0, 0, 0, 0, 0, 0, 0);
 // @ts-ignore
-@lazy let adaa = v128(0,  1,  2,  3,
-    12, 13, 14, 15,
-    0,  0,  0,  0,
-    0,  0,  0,  0);
+@lazy const bczzSwizzle: v128 = v128(4, 5, 6, 7, 8, 9, 10, 11, 0, 0, 0, 0, 0, 0, 0, 0);
 // @ts-ignore
-@lazy let bcaa = v128(4, 5, 6, 7, 8, 9, 10, 11,
-    0, 0, 0, 0, 0, 0, 0, 0);
+@lazy const zzbcSwizzle: v128 = v128(0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 6, 7, 8, 9, 10, 11);
+// @ts-ignore
+@lazy const baccSwizzle: v128 = v128(4, 5, 6, 7, 0, 1, 2, 3, 8, 9, 10, 11, 8, 9, 10, 11);
+// @ts-ignore
+@lazy const cbabSwizzle: v128 = v128(8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7);
+// @ts-ignore
+@lazy const zbdzSwizzle: v128 = v128(0, 0, 0, 0, 4, 5, 6, 7, 12, 13, 14, 15, 0, 0, 0, 0);
+// @ts-ignore
+@lazy const zdczSwizzle: v128 = v128(0, 0, 0, 0, 12, 13, 14, 15, 8, 9, 10, 11, 0, 0, 0, 0);
+// @ts-ignore
+@lazy const cbzzSwizzle: v128 = v128(8, 9, 10, 11, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0);
+
 export function concat(m: Matrix, n: Matrix): void {
     if (ASC_FEATURE_SIMD) {
         let mv128 = v128.load(changetype<usize>(m));
         let nv128 = v128.load(changetype<usize>(n));
-        let abcd = v128.mul(
-            v128.mul(mv128, adMask),
-            v128.mul(nv128, adMask),
-        );
-        // two lanes, tx, ty
-        let mtxy = v128.load_ext<f32>(changetype<usize>(m), offsetof<Matrix>("tx"));
-        let ntxy = v128.load_ext<f32>(changetype<usize>(n), offsetof<Matrix>("tx"));
-        let nad = v128.extend_low<f32>(v128.swizzle(nv128, adaa));
-        let mbc = v128.extend_low<f32>(v128.swizzle(mv128, bcaa));
-        let nbc = v128.extend_low<f32>(v128.swizzle(nv128, bcaa));
+        let mtxty = v128.load_zero(changetype<usize>(m), offsetof<Matrix>("tx"));
+        mtxty = v128.load_lane<f32>(changetype<usize>(m), mtxty, 1, offsetof<Matrix>("ty"));
+        let ntxty = v128.load_zero(changetype<usize>(n), offsetof<Matrix>("tx"));
+        ntxty = v128.load_lane<f32>(changetype<usize>(n), ntxty, 1, offsetof<Matrix>("ty"));
 
         // TODO: Complete this operation
+        let abcd = v128.mul<f32>(
+            v128.mul(mv128, nv128),
+            f32x4(1, 0, 0, 1)
+        );
+        let txty = v128.add<f32>(
+            v128.mul<f32>(
+                mtxty,
+                v128.swizzle(nv128, adzzSwizzle)
+            ),
+            ntxty,
+        );
+
+        // select the bc lanes in the first two lanes
+        let mbc = v128.mul<f32>(v128.swizzle(mv128, bczzSwizzle), f32x4(1, 1, 0, 0));
+        // select the bc lanes in the last two lanes
+        let nbc = v128.mul<f32>(v128.swizzle(mv128, zzbcSwizzle), f32x4(0, 0, 1, 1));
+        // add them together
+        let mbcnbc = v128.add<f32>(mbc, nbc);
+        if (v128.any_true(v128.ne(mbcnbc, f32x4(0, 0, 0, 0)))) {
+            abcd = v128.add<f32>(
+                abcd,
+                v128.add<f32>(
+                    v128.mul<f32>(
+                        v128.swizzle(mv128, baccSwizzle),
+                        v128.swizzle(nv128, cbabSwizzle),
+                    ),
+                    v128.mul<f32>(
+                        v128.mul<f32>(
+                            v128.swizzle(mv128, zbdzSwizzle),
+                            v128.swizzle(nv128, zdczSwizzle),
+                        ),
+                        f32x4(0, 1, 1, 0),
+                    ),
+                )
+            );
+            txty = v128.add<f32>(
+                txty,
+                v128.mul<f32>(
+                    v128.add<f32>(
+                        v128.mul<f32>(
+                            mtxty,
+                            v128.swizzle(nv128, cbzzSwizzle),
+                        ),
+                        ntxty,
+                    ),
+                    f32x4(1, 1, 0, 0)
+                ),
+            )
+        }
+        v128.store(changetype<usize>(m), abcd);
+        v128.store_lane<f32>(changetype<usize>(m), txty, 0, offsetof<Matrix>("tx"));
+        v128.store_lane<f32>(changetype<usize>(m), txty, 1, offsetof<Matrix>("ty"));
+
     } else {
         let a =  m.a * n.a;
         let b =  0.0;
@@ -81,9 +135,9 @@ export function concat(m: Matrix, n: Matrix): void {
     
         if (m.b !== 0.0 || m.c !== 0.0 || n.b !== 0.0 || n.c !== 0.0) {
             a  += m.b * n.c;
-            d  += m.c * n.b;
             b  += m.a * n.b + m.b * n.d;
             c  += m.c * n.a + m.d * n.c;
+            d  += m.c * n.b;
             tx += m.ty * n.c;
             ty += m.tx * n.b;
         }
